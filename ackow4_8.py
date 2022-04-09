69ba1b4cdf9db448704806a65405c6c94f483cb2
薛展鹏 <xzp01@foxmail.com>
2022-04-09 12:20:50 +0800

    master

---
+++ ackow4_8.py
@@ -0,0 +1,18 @@
+# values=[]
+# for x in range(1,101):
+#     values.append(x)
+
+# print(values)
+
+
+# values =[x for x in range(1,101)]
+# print(values)
+
+
+#三目运算符
+# a=1
+# b=2
+# c=5 if a<b else 0
+# print(c)
+
+
